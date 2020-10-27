ffadb240f205d46f8981388188a9abc52e12c28a
Zachary Belford <belfordz66@gmail.com>
2020-10-27 16:47:06 -0700

    fix: browser build mock fs not working still

--- src/index-web.ts
+++ src/index-web.ts
@@ -2,7 +2,7 @@ import buildReferenceResolver from "./reference-resolver";
 import fetch from "isomorphic-fetch";
 
 export default buildReferenceResolver(fetch, {
-  access: () => Promise.resolve(false),
-  readFile: () => Promise.resolve(""),
+  access: (a: any, b: any, cb: (e: Error) => any) => cb(new Error("cant resolve file refs in a browser... yet")),
+  readFile: (a: any, b: any, cb: () => any) => { return cb(); },
   constants: { F_OK: 0, R_OK: 0 } //tslint:disable-line
 });
