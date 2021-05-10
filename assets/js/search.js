ab800e7ea96e488bcc61000ab4f3791dc5e817b3
BelfordZ <semantic-release-bot@martynus.net>
2021-05-10 21:34:21 +0000

    github pages release

---
+++ assets/js/search.js
@@ -0,0 +1,1 @@
+window.searchData = {"kinds":{"1024":"Property"},"rows":[{"id":0,"kind":1024,"name":"default","url":"modules.html#default","classes":"tsd-kind-property"}],"index":{"version":"2.3.9","fields":["name","parent"],"fieldVectors":[["name/0",[0,2.877]],["parent/0",[]]],"invertedIndex":[["default",{"_index":0,"name":{"0":{}},"parent":{}}]],"pipeline":[]}}
